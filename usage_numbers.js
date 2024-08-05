52c6fd847b6752fad62ac2882c47427b52f1bdb7
rahulnaik92 <teamrahul.naik@gmail.com>
2024-08-05 11:11:48 +0530

    updated code

---
+++ usage_numbers.js
@@ -0,0 +1,25 @@
+//numbers
+
+let score=50
+console.log(score, typeof score)
+
+//Mathmatical Operator * / % ** +-
+
+let result = score/2 // Give Qoutient
+
+console.log(result)
+
+let newResult = score%2 // Give Remainder
+console.log(newResult)
+
+
+let updatedResult = score *2+(4*3)-8/2%4;
+console.log(updatedResult)
+//Using priority and Precedence
+
+//1. () Bracket
+//2. ** Power Operator
+//3 * / % (Form Left to Right)
+//4 + - (From Left to Right)
+
+//
