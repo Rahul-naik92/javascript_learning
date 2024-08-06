fb3edfd3370e0971c2eed7cf631ad545163d6d77
rahulnaik92 <teamrahul.naik@gmail.com>
2024-08-06 23:33:16 +0530

    type conversation

--- usage_type_conversation.js
+++ usage_type_conversation.js
@@ -9,3 +9,11 @@ console.log(stringType, typeof stringType)
 let numberType = Number(stringType)
 
 console.log(numberType, typeof numberType)
+
+//Imp note
+//In number method passing String must be of numeri values
+
+let numberConversion = 45
+let stringConversion = String(numberConversion)
+
+console.log(stringConversion, typeof stringConversion)
