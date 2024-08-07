dabaf22eb172de53dfdbe02e4caf92c38f6fb0d8
rahulnaik92 <teamrahul.naik@gmail.com>
2024-08-07 12:01:50 +0530

    for loop

---
+++ usage_control_flow.js
@@ -0,0 +1,27 @@
+//control FLow
+
+
+
+//For loops
+
+//Run the specific task 100 times
+
+
+//for loops
+
+for(let i=0; i<=100;i++){
+    console.log("Run the task",i)
+}
+
+//uscase of the loop
+
+let dished=['Biryani', 'Bhallay', 'pani-puri', 'Chapati'];
+
+for(let i=0; i<dished.length; i++){
+    console.log(dished[i])
+}
+
+//iteration and value of i
+//0,1,2,3
+
+//while loop
