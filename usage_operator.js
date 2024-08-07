6ab77a63ef2254d6d7bfd1ba7cbc773318ef3652
rahulnaik92 <teamrahul.naik@gmail.com>
2024-08-07 12:50:43 +0530

    operator

--- usage_operator.js
+++ usage_operator.js
@@ -4,3 +4,26 @@
 //>, >=
 //<, <=
 
+
+//logical operator
+//AND &&, or ||, Not !
+
+//example
+
+let password ="qwerty@"
+if(password.length>5 && password.includes('@')){
+    console.log("Password Strenght is Strong")
+}
+
+//not operator !
+
+let statusButton =false
+
+if(!statusButton){
+console.log("Button is on", statusButton)
+}
+
+//Priorities
+//Not
+
+//and, OR From left to right
