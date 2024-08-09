8baff7558630be59c6d5c7597732ec6b786aef83
rahulnaik92 <teamrahul.naik@gmail.com>
2024-08-09 13:49:01 +0530

    function

---
+++ usage_function.js
@@ -0,0 +1,34 @@
+//Functions
+
+//It is simply a piece of code which we can use in our program 
+//many time.
+
+//It is just like a variable holding some piece of code
+
+//Declaration.
+
+function nameOffFunction(){
+    console.log("Name of function");
+}
+//call Function
+nameOffFunction()
+
+//Function Expression
+
+
+let fun = function(){
+    //also called anonyms function
+
+    console.log("Anonums Function")
+}
+
+fun()
+
+//Real Time example:
+
+let invitation=function(name) {
+
+    console.log(`Welcome ${name} you are invited on our event.`)
+}
+
+invitation('Rahul')
